a972e0be38bca2fc1af9fe110c683e52b4e07547
Uchechukwu-Udefi <udefihenry@gmail.com>
2025-05-21 11:17:08 +0200

    Create initial state verification

---
+++ scripts.js
@@ -0,0 +1,10 @@
+//initial state verification set value to 0 and result log to console.
+const getState = () => {
+    const state = {
+        value: 0,
+        };
+        return state;
+};
+console.log(getState());
+
+
