80d54730d0e5fb4195b1fd76b8a39484acf46317
Uchechukwu-Udefi <udefihenry@gmail.com>
2025-05-22 12:24:41 +0200

    Create and update actions JS file

---
+++ store/actions.js
@@ -0,0 +1,31 @@
+//increment function
+export const add = () => {
+  state = {
+    ...state,
+    value: state.value + 1,
+  };
+  return state;
+};
+
+//decrement function
+export const subtract = () => {
+    state = {
+        ...state,
+        value: state.value - 1,
+    };
+    return state;
+};
+
+//reset function
+export const reset = () => {
+    state = {
+        ...state,
+        value: 0,
+    };
+    return state;
+};
+
+//log function
+export const get = (state, key) => {
+    return state[key];
+};
