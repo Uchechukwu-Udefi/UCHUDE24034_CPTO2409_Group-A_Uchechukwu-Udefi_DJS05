42101fe0bf147b01d90a36e2b024328580a23db3
Uchechukwu-Udefi <udefihenry@gmail.com>
2025-05-22 10:21:47 +0200

    Create increment function and test

--- scripts.js
+++ scripts.js
@@ -1,5 +1,4 @@
 //initial state verification set value to 0 and result log to console.
-
 let state = {
   value: 0,
 };
@@ -8,3 +7,16 @@ const getState = () => state;
 
 console.log(getState()); 
 
+//increment function
+const add = () => {
+  state = {
+    ...state,
+    value: state.value + 1,
+  };
+  return state;
+};
+
+//increment function test
+console.log(add()); 
+console.log(add());  
+console.log(getState());
