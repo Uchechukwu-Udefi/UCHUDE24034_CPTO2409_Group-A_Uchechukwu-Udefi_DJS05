799272f64f45a91f3f99243ed7d1e944c16aee9b
Uchechukwu-Udefi <udefihenry@gmail.com>
2025-05-25 12:15:20 +0200

    Update documentation and comment

--- store/reducer.js
+++ store/reducer.js
@@ -1,4 +1,23 @@
-// reducer.js
+/**
+ * @typedef {Object} State
+ * @property {number} count - The current tally count
+ * @property {string} [comment] - An optional message (e.g., used after a reset case)
+ */
+
+/**
+ * @typedef {Object} Action
+ * @property {string} type - The action type ('ADD', 'SUBTRACT', 'RESET')
+ */
+
+/**
+ * Reducer function that updates the state of a tally counter based on the dispatched action.
+ *
+ * @function
+ * @param {State} [state={ count: 0 }] - The current state of the counter
+ * @param {Action} action - The action to process
+ * @returns {State} The new state after applying the action
+ */
+
 export function tallyReducer(state = { count: 0 }, action) {
 
   switch (action.type) {
@@ -9,8 +28,11 @@ export function tallyReducer(state = { count: 0 }, action) {
       return { count: state.count - 1 };
 
     case 'RESET':
-      return { count: 0 };
-      
+      return { 
+        count: 0 ,
+        comment: 'Count reset to zero'
+      };
+
     default:
       return state;
   };
